5935414e5147304b3550b14f6d4240dd68c581e5
jiewei <13751879210@163.com>
2019-08-06 16:01:48 +0800

    修改杂项项目名

--- miscellaneous-service/src/main/java/com/zjw/zerer/miscellaneous/MiscellaneousServiceApplication.java
+++ miscellaneous-service/src/main/java/com/zjw/zerer/miscellaneous/MiscellaneousServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiscellaneousApplication {
+public class MiscellaneousServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MiscellaneousApplication.class, args);
+        SpringApplication.run(MiscellaneousServiceApplication.class, args);
     }
 
 }
