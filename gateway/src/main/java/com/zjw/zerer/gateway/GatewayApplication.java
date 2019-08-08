e53f75a8d09c958c27f730f62c74f69a7c4d5077
jiewei <13751879210@163.com>
2019-08-08 17:03:14 +0800

    增加网关配置

--- gateway/src/main/java/com/zjw/zerer/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/zjw/zerer/gateway/GatewayApplication.java
@@ -1,13 +1,12 @@
 package com.zjw.zerer.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+
 @EnableZuulProxy
+@SpringCloudApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
