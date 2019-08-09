bce468a8585f760b4ba089218769715c099f5efb
jiewei <13751879210@163.com>
2019-08-09 18:26:39 +0800

    add spring cloud config

--- zerer-config/src/main/java/com/zjw/zerer/config/ConfigApplication.java
+++ zerer-config/src/main/java/com/zjw/zerer/config/ConfigApplication.java
@@ -2,7 +2,11 @@ package com.zjw.zerer.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
 
